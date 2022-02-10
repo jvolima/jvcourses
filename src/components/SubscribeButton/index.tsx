3360d64701cc5b4e215dd92002497ed6cae8e32d
jvolima <jvolima2004@gmail.com>
2022-02-10 11:15:38 -0300

    Redirecionamento para a checkout session

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import { Button } from "./styles";
 
 interface SubscribeButtonProps {
@@ -5,8 +8,28 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession()
+
+  async function handleSubscribeButton() {
+    if(!session) {
+      signIn("github")
+      return
+    }
+
+    try {
+      const response = await api.post("/subscribe")
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch (error) {
+      alert(error)
+    }
+  }
+
   return (
-    <Button>
+    <Button onClick={handleSubscribeButton}>
       Subscribe now
     </Button>
   )
